78cf29c22f9b2c7dec7fa95982e06dacfe39775b
jakob.valen <jakob.valen@fullscript.com>
2025-09-10 14:29:43 -0700

    Use vcluster version 0.27.0

--- Formula/vcluster.rb
+++ Formula/vcluster.rb
@@ -2,43 +2,42 @@ class Vcluster < Formula
   desc "Creates fully functional virtual k8s cluster inside host k8s cluster's namespace"
   homepage "https://www.vcluster.com"
   url "https://github.com/loft-sh/vcluster.git",
-      tag:      "v0.19.7",
-      revision: "75c742cf1bbca367394ee04e3ee9f9664ae0bc56"
+      tag:      "v0.27.0",
+      revision: "d23f473e89c0caff7b103758e03886dc99dad1f8"
   license "Apache-2.0"
   head "https://github.com/loft-sh/vcluster.git", branch: "main"
+
+  # Upstream creates releases that use a stable tag (e.g., `v1.2.3`) but are
+  # labeled as "pre-release" on GitHub before the version is released, so it's
+  # necessary to use the `GithubLatest` strategy.
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    root_url "https://github.com/Fullscript/homebrew-tools/releases/download/bottles"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "caec042fe797eeaf0b377c8e4c9f2428dfbedc848be1d78c7362d37951170015"
+  end
+
+  conflicts_with "homebrew/core/vcluster"
 
   depends_on "go" => :build
   depends_on "helm"
   depends_on "kubernetes-cli"
 
-  conflicts_with "homebrew/core/vcluster"
-
-  bottle do
-    root_url "https://github.com/Fullscript/homebrew-tools/releases/download/bottles"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "63b9771c605e21ddcad311ebf5f56d1232b729f64cd83a19e926c5e76a3c2883"
-  end
-
   def install
-    ldflags = %W[
-      -s -w
-      -X main.commitHash=#{Utils.git_head}
-      -X main.buildDate=#{time.iso8601}
-      -X main.version=#{version}
-    ]
+    ldflags = "-s -w -X main.commitHash=#{Utils.git_head} -X main.buildDate=#{time.iso8601} -X main.version=#{version}"
     system "go", "generate", "./..."
-    system "go", "build", "-mod", "vendor", *std_go_args(ldflags:), "./cmd/vclusterctl/main.go"
+    system "go", "build", "-mod", "vendor", *std_go_args(ldflags:), "./cmd/vclusterctl"
+
     generate_completions_from_executable(bin/"vcluster", "completion")
   end
 
   test do
-    help_output = "vcluster root command"
-    assert_match help_output, shell_output("#{bin}/vcluster --help")
+    assert_match version.to_s, shell_output("#{bin}/vcluster version")
 
-    create_output = "there is an error loading your current kube config " \
-                    "(invalid configuration: no configuration has been provided, " \
-                    "try setting KUBERNETES_MASTER environment variable), " \
-                    "please make sure you have access to a kubernetes cluster and the command " \
-                    "`kubectl get namespaces` is working"
-    assert_match create_output, shell_output("#{bin}/vcluster create vcluster -n vcluster --create-namespace 2>&1", 1)
+    output = shell_output("#{bin}/vcluster create vcluster -n vcluster --create-namespace 2>&1", 1)
+    assert_match "try setting KUBERNETES_MASTER environment variable", output
   end
 end
